477c1031f2fa2e1ed82976b96e8d7c5e995cf26a
Brenno Brossi <brenno_workflow@outlook.com>
2024-04-30 19:51:38 -0300

    test5 - env on render

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -32,10 +32,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -105,6 +104,7 @@ DATABASES = {
     }
 }
 
+database_url = os.getenv("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse("postgres://django_postgresql_evcx_user:Z39IoERquYBYndb3GEblSndv01sOakJq@dpg-com1o221hbls73998950-a.oregon-postgres.render.com/django_postgresql_evcx")
 
 # postgres://django_postgresql_evcx_user:Z39IoERquYBYndb3GEblSndv01sOakJq@dpg-com1o221hbls73998950-a/django_postgresql_evcx
